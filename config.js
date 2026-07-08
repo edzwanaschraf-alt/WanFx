1f6e7e042e0ac8582624021daf7418d42dc28e47
edzwanaschraf-alt <edzwanaschraf@gmail.com>
2026-07-08 18:44:32 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyCFCF7_yOZ_wRATnaNx1X3LRmsyetQe4RI",
-  authDomain: "wanfx-66c65.firebaseapp.com",
-  projectId: "wanfx-66c65",
-  storageBucket: "wanfx-66c65.firebasestorage.app",
-  messagingSenderId: "44515785173",
-  appId: "1:44515785173:web:e7eaf23b247ed4c88ba55c",
-  measurementId: "G-9CHB5PRJ7B"
-};
